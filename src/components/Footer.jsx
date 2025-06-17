eb5f69095aa261007df0c7f5295dd9365df53961
sourabh gautam <gautamsourabh98.com>
2025-06-17 23:24:30 +0530

    fix: update project name in package-lock and enhance footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,29 @@
 import { FaLinkedin, FaGithub, FaBehance } from "react-icons/fa";
-import { HiOutlineGlobeAlt } from "react-icons/hi"; // For portfolio icon "react-icons/fa";
+import { HiOutlineGlobeAlt } from "react-icons/hi";
 
 const socialLinks = [
-    { href: "https://www.linkedin.com/in/sourabhgautam/", icon: <FaLinkedin /> },
+  { href: "https://www.linkedin.com/in/sourabhgautam/", icon: <FaLinkedin /> },
   { href: "https://github.com/sourabhgautam98", icon: <FaGithub /> },
-  { href: "https://portfolio-sourabh-gautam.vercel.app/", icon: <HiOutlineGlobeAlt /> }, // Replace with actual portfolio link
+  { href: "https://portfolio-sourabh-gautam.vercel.app/", icon: <HiOutlineGlobeAlt /> },
   { href: "https://www.behance.net/sourabhgautam98", icon: <FaBehance /> },
 ];
 
 const Footer = () => {
   return (
-    <footer className="w-screen bg-[#FDBA74] py-4 text-black">
+    <footer className="w-screen bg-black py-10 text-white">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
           ©SourabhGautam2025. All rights reserved
         </p>
 
-        <div className="flex justify-center gap-4  md:justify-start">
+        <div className="flex justify-center gap-10 md:justify-start">
           {socialLinks.map((link, index) => (
             <a
               key={index}
               href={link.href}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-black transition-colors duration-500 ease-in-out hover:text-white"
+              className="text-white text-2xl transition-colors duration-500 ease-in-out hover:text-orange-400"
             >
               {link.icon}
             </a>
